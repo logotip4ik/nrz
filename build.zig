4e4fbeaa8c18d49710a339cec89616a419fdd891
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-06-27 20:39:54 +0300

    chore: remove not used module

--- build.zig
+++ build.zig
@@ -22,18 +22,10 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const fsWalker = b.addStaticLibrary(.{
-        .name = "nrz_fsWalker",
-        .root_source_file = b.path("src/fs-walker.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
     b.installArtifact(string);
-    b.installArtifact(fsWalker);
 
     const exe = b.addExecutable(.{
         .name = "nrz",
