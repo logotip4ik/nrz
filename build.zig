c899202633782437e64bb4ec7bd07e2b816c7d66
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-07-01 12:02:16 +0300

    fix: add string and suggest modules to test exe

--- build.zig
+++ build.zig
@@ -87,6 +87,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe_unit_tests.root_module.addImport("string", string);
+    exe_unit_tests.root_module.addImport("suggest", suggest);
+
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
